d8c7404c5024b2dc2ee0a231234a54c364b06380
Pi-sereyvathanak <vathanakpi@gmail.com>
2025-12-25 08:35:26 +0700

    Modified

--- src/Lab6/Subject.java
+++ src/Lab6/Subject.java
@@ -1,8 +1,32 @@
 
 class SubjectData {
-    String name;
-    int totalCredits;
-    int totalHours;   // fixed typo
+    private String name;
+    private int totalCredits;
+    private int totalHours;   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+    public void setTotalCredits(int totalCredits) {
+        this.totalCredits = totalCredits;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(int totalHours) {
+        this.totalHours = totalHours;
+    }
 
     @Override
     public String toString() {
@@ -16,9 +40,9 @@ class SubjectData {
 public class Subject {
     public static void main(String[] args) {
         SubjectData x = new SubjectData();
-        x.name = "Introduction to Software Engineering";
-        x.totalCredits = 3;
-        x.totalHours = 60;
+        x.setName("Introduction to SE");
+        x.setTotalCredits(3);
+        x.setTotalHours(48);
 
         System.out.println(x);
     }
