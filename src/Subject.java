993e377334cc5f616b05b594a49167da4296f2f1
Pi-sereyvathanak <vathanakpi@gmail.com>
2025-12-25 08:46:32 +0700

    New update

---
+++ src/Subject.java
@@ -0,0 +1,49 @@
+
+class SubjectData {
+    private String name;
+    private int totalCredits;
+    private int totalHours;   
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+    public void setTotalCredits(int totalCredits) {
+        this.totalCredits = totalCredits;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(int totalHours) {
+        this.totalHours = totalHours;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Subject Name: %s, Total Credits: %d, Total Hours: %d",
+            name, totalCredits, totalHours
+        );
+    }
+}
+
+public class Subject {
+    public static void main(String[] args) {
+        SubjectData x = new SubjectData();
+        x.setName("Introduction to SE");
+        x.setTotalCredits(3);
+        x.setTotalHours(48);
+
+        System.out.println(x);
+    }
+}
