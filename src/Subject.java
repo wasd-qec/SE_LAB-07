5bb1f0327859890327bc1cd89c5128776b0873a8
kolboth <rithchankolboth@gmail.com>
2025-12-27 18:17:55 +0700

    2 and 3

--- src/Subject.java
+++ src/Subject.java
@@ -1,33 +1,87 @@
+/**
+ * Subject class represents an academic subject with name, credits, and hours.
+ * 
+ * @author Your Name
+ * @version 1.0
+ * @since 2025-12-27
+ */
 class Subject{
     String name;
     int totalcredits;
     int TotalHours;
+    
+    /**
+     * Returns string representation of the subject.
+     * 
+     * @return formatted string with name, credits, and hours
+     */
     public String toString(){
         return String.format("%s %d %d", name, totalcredits, TotalHours);
     }
+    
+    /**
+     * Gets the name of the subject.
+     * 
+     * @return the subject name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the subject.
+     * 
+     * @param name the subject name to set
+     * @throws IllegalArgumentException if name is null or empty
+     */
     public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
         this.name = name;
     }
 
+    /**
+     * Gets the total credits for the subject.
+     * 
+     * @return the total credits
+     */
     public int getTotalcredits() {
         return totalcredits;
     }
 
+    /**
+     * Sets the total credits for the subject.
+     * 
+     * @param totalcredits the total credits to set
+     */
     public void setTotalcredits(int totalcredits) {
         this.totalcredits = totalcredits;
     }
 
+    /**
+     * Gets the total hours for the subject.
+     * 
+     * @return the total hours
+     */
     public int getTotalHours() {
         return TotalHours;
     }
 
+    /**
+     * Sets the total hours for the subject.
+     * 
+     * @param totalHours the total hours to set
+     */
     public void setTotalHours(int totalHours) {
         TotalHours = totalHours;
     }
+    
+    /**
+     * Main method to test the Subject class.
+     * 
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Subject subject = new Subject();
         subject.name = "Mathematics";
