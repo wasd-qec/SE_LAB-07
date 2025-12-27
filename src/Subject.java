78a9bc3869f05ec7133e598651a926735d5e3bfb
kolboth <rithchankolboth@gmail.com>
2025-12-27 16:58:16 +0700

    2nd

--- src/Subject.java
+++ src/Subject.java
@@ -5,6 +5,29 @@ class Subject{
     public String toString(){
         return String.format("%s %d %d", name, totalcredits, TotalHours);
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalcredits() {
+        return totalcredits;
+    }
+
+    public void setTotalcredits(int totalcredits) {
+        this.totalcredits = totalcredits;
+    }
+
+    public int getTotalHours() {
+        return TotalHours;
+    }
+
+    public void setTotalHours(int totalHours) {
+        TotalHours = totalHours;
+    }
     public static void main(String[] args) {
         Subject subject = new Subject();
         subject.name = "Mathematics";
