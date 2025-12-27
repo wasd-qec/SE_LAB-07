3acd47d1e0766fb854a76780b1a6d619ddcf191a
Pi-sereyvathanak <vathanakpi@gmail.com>
2025-12-27 19:23:01 +0700

    add attendance score

--- src/Lab6/Subject.java
+++ src/Lab6/Subject.java
@@ -1,38 +1,72 @@
-
 class SubjectData {
     private String name;
     private int totalCredits;
-    private int totalHours;   
+    private int totalHours;
 
+    private int presentCount;
+    private int lateCount;
+    private int absentCount;
+
+    // Existing Getters/Setters
     public String getName() {
-        return name;
-    }
-
+         return name; 
+        }
     public void setName(String name) {
-        this.name = name;
-    }
+         this.name = name; 
+        }
 
     public int getTotalCredits() {
-        return totalCredits;
-    }
-
-    public void setTotalCredits(int totalCredits) {
-        this.totalCredits = totalCredits;
+         return totalCredits; 
+        }
+    public void setTotalCredits(int totalCredits) { 
+        this.totalCredits = totalCredits; 
     }
 
     public int getTotalHours() {
-        return totalHours;
+         return totalHours; 
+        }
+    public void setTotalHours(int totalHours) {
+         this.totalHours = totalHours; 
+        }
+
+    public int getPresentCount() {
+         return presentCount; 
+        }
+    public void setPresentCount(int presentCount) {
+         this.presentCount = presentCount; 
+        }
+
+    public int getLateCount() {
+         return lateCount; 
+        }
+    public void setLateCount(int lateCount) 
+    { this.lateCount = lateCount; 
+
     }
 
-    public void setTotalHours(int totalHours) {
-        this.totalHours = totalHours;
+    public int getAbsentCount() {
+         return absentCount; 
+        }
+    public void setAbsentCount(int absentCount) {
+         this.absentCount = absentCount; 
+        }
+
+    public double calculateAttendanceScore() {
+        int totalSessions = presentCount + lateCount + absentCount;
+        
+        // Prevent division by zero if no attendance is recorded yet
+        if (totalSessions == 0) return 0.0;
+
+        double score = (presentCount + (lateCount * 0.5)) / totalSessions;
+        return score * 100;
     }
 
     @Override
     public String toString() {
         return String.format(
-            "Subject Name: %s, Total Credits: %d, Total Hours: %d",
-            name, totalCredits, totalHours
+            "Subject: %s\nCredits: %d\nHours: %d\nAttendance Score: %.2f%% (P: %d, L: %d, A: %d)",
+            name, totalCredits, totalHours, calculateAttendanceScore(), 
+            presentCount, lateCount, absentCount
         );
     }
 }
@@ -43,6 +77,13 @@ public class Subject {
         x.setName("Introduction to SE");
         x.setTotalCredits(3);
         x.setTotalHours(45);
+
+        // Setting attendance data
+        x.setPresentCount(10);
+        x.setLateCount(2);
+        x.setAbsentCount(1);
+        
+        System.out.println("Attendance score: "+ x.calculateAttendanceScore() + "%");
 
         System.out.println(x);
     }
