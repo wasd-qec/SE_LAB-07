2ef7ec8c852e50a61b86c0bd245e5b610e51414f
Seavhong CHEA <cseavhong22@gmail.com>
2025-12-30 15:49:27 +0700

    add javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Session"},{"p":"<Unnamed>","l":"Subject"}];updateSearchResults();
